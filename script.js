f3ced0f8a8068d1a9151aa9deb11001a2bf59264
bdkautzman <briankautzman@gmail.com>
2024-08-08 10:52:25 -0500

    Add card generation login, remove temp card structure

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+const libraryVis = document.querySelector(".library");
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -14,10 +16,51 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(title, author, pages, read) {
     newBook = new Book(title, author, pages, read);
-
     myLibrary.push(newBook);
+    refreshLibrary();
+}
 
-    for(book in myLibrary){
-        
+function refreshLibrary() {
+    const cards = libraryVis.childNodes;
+
+    for(const node of cards){
+        node.remove();
+    }
+
+    for(const book of myLibrary){
+        newCard = genBookCard(book);
+        libraryVis.appendChild(newCard);
+        console.log(book);
     }
 }
+
+function genBookCard(book) {
+    const card = document.createElement("div");
+    card.className = "book";
+
+    const title = document.createElement("div");
+    title.textContent = book.title;
+    card.appendChild(title);
+
+    const author = document.createElement("div");
+    author.textContent = book.author;
+    card.appendChild(author);
+
+    const pages = document.createElement("div");
+    pages.textContent = book.pages;
+    card.appendChild(pages);
+
+    const read = document.createElement("div");
+    if (book.read) {
+        read.textContent = "Read";
+    }else {
+        read.textContent = "Not read";
+    }
+    card.appendChild(read);
+    
+    return card;
+}
+
+addBookToLibrary("A Song of Fire and Ice", "George R R Martin", 1653, true);
+addBookToLibrary("The Name of the Wind", "Patrick Rothfuss", 932, true);
+addBookToLibrary("Hitchhikers Guide to the Galaxy", "????", 641, false);
